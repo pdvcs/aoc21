f3f106421a4e0c7b8f9b1ad1952fd598d993f210
Prasenjeet Dutta <prasenjeet.dutta@gmail.com>
2021-12-12 04:30:16 +0000

    code for day 2 and 3

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Advent of Code 2021
 
-[Advent of Code 2021](https://adventofcode.com/2021) In Kotlin.
+[Advent of Code 2021](https://adventofcode.com/2021) in Kotlin.
+
+Use Gradle 7.x to build this. Tested with Gradle 7.3.
 
 **Build**: `gradle build`
 
-**Run**: `gradle run`
+**Run**: `gradle run` or run `net.pdutta.aoc21.AoCKt`.
--- src/test/kotlin/net/pdutta/aoc21/AocTest.kt
+++ src/test/kotlin/net/pdutta/aoc21/AocTest.kt
@@ -4,17 +4,36 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 internal class AoCTest {
-    private val report = "199 200 208 210 200 207 240 269 260 263"
+    private val day1input = "199 200 208 210 200 207 240 269 260 263".split(" ").map { it.toInt() }
+    private val day2input = """
+                                forward 5
+                                down 5
+                                forward 8
+                                up 3
+                                down 8
+                                forward 2
+                            """.trimIndent()
+    private val day3input = "00100 11110 10110 10111 10101 01111 00111 11100 10000 11001 00010 01010".split(" ")
 
     @Test
-    fun testProblem01() {
-        val expected = 7
-        assertEquals(expected, largerThanPrevious(toList(report)))
+    fun testDay01() {
+        println("== testDay01 ==")
+        assertEquals(7, measureSweep(day1input))
+        assertEquals(5, measureSweepWithSlidingWindow(day1input))
     }
 
     @Test
-    fun testProblem02() {
-        val expected = 5
-        assertEquals(expected, slidingWindow(toList(report)))
+    fun testDay02() {
+        println("== testDay02 ==")
+        assertEquals(150, diveCalc(day2input.split("\n")))
+        assertEquals(900, diveCalcWithAim(day2input.split("\n")))
     }
+
+    @Test
+    fun testDay03() {
+        println("== testDay03 ==")
+        assertEquals(198, binaryDiagnostic(day3input))
+        assertEquals(230, lifeSupportDiagnostic(day3input))
+    }
+
 }
